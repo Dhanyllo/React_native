69a9e798ee3f923d3a7080b8c8c2b46a05251852
Dhanyllo <danielotchere84@gmail.com>
2025-03-25 11:43:41 +0000

    Added my TodoList

---
+++ CrudApp/constants/MenuItems.js
@@ -0,0 +1,52 @@
+export const MENU_ITEMS = [
+  {
+    "id": 1,
+    "title": "Espresso",
+    "description": "Strong, concentrated coffee."
+  },
+  {
+    "id": 2,
+    "title": "Americano",
+    "description": "Espresso with added hot water for a lighter flavor."
+  },
+  {
+    "id": 3,
+    "title": "Latte",
+    "description": "Espresso with steamed milk and a light layer of foam."
+  },
+  {
+    "id": 4,
+    "title": "Cappuccino",
+    "description": "Equal parts espresso, steamed milk, and foam."
+  },
+  {
+    "id": 5,
+    "title": "Mocha",
+    "description": "Espresso mixed with chocolate syrup and steamed milk."
+  },
+  {
+    "id": 6,
+    "title": "Macchiato",
+    "description": "Espresso topped with a small amount of foamed milk."
+  },
+  {
+    "id": 7,
+    "title": "Flat White",
+    "description": "Espresso with steamed milk but no foam."
+  },
+  {
+    "id": 8,
+    "title": "Affogato",
+    "description": "Espresso poured over a scoop of vanilla ice cream."
+  },
+  {
+    "id": 9,
+    "title": "Cortado",
+    "description": "Equal parts espresso and warm milk for a balanced taste."
+  },
+  {
+    "id": 10,
+    "title": "Ristretto",
+    "description": "A more concentrated espresso with less water."
+  }
+];
